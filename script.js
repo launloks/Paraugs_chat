d53ab2a4672db74b425290cd2bd9abb97a58464f
launloks <lauris.kocins@limbazugimnazija.lv>
2024-04-15 15:40:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //console.log(1)
 function sentmessage() {
     console.log ('sentmessage()works');
+    let message = document.querySelector('message').value;
+    let messages = document.querySelector('chatmessage');
+    messages.innerHTML = messages.innerHTML+'<br>'+message.value;
+
+    
 }
